ed55bef84ef8ea0667a6e9a3937cd6c0136dbf51
Juan Guillermo Rivera Orozco <guillerivera32@gmail.com>
2017-09-23 22:22:28 -0500

    Basic data base module

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -1,6 +1,19 @@
 'use strict'
 
+const setupDatabase = require('./lib/db')
+const setupAgentModel = require('./models/agent')
+const setupMetricModel = require('./models/metric')
+
 module.exports = async function (config) {
+  const sequalize = setupDatabase(config)
+  const AgentModel = setupAgentModel(config)
+  const MetricModel = setupMetricModel(config)
+
+  AgentModel.hasMany(MetricModel)
+  MetricModel.belongsTo(AgentModel)
+
+  await sequalize.authenticate()
+
   const Agent = {}
   const Metric = {}
 
