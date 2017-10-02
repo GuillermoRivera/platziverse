f0538d033d5eb67ff6d163266de95a4fdba78ef8
Juan Guillermo Rivera Orozco <guillerivera32@gmail.com>
2017-10-01 23:22:29 -0500

    Created warning about erasing data

--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -2,8 +2,24 @@
 
 const db = require('./')
 const debug = require('debug')('platziverse:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+
+const prompt = inquirer.createPromptModule()
 
 async function setup () {
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will destroy your database, are you sure?'
+    }
+  ])
+
+  if (!answer.setup) {
+    return console.log('Nothing happened :)')
+  }
+
   const config = {
     database: process.env.DB_NAME || 'platziverse',
     username: process.env.DB_USER || 'platzi',
@@ -20,7 +36,7 @@ async function setup () {
 }
 
 function handleFatalError (error) {
-  console.log(error.message)
+  console.log(`${chalk.red('[fatal error]')} ${error.message}`)
   console.log(error.stack)
   process.exit(1)
 }
